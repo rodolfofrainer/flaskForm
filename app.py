0df39e6b29dba4c2c2ddc9473eafdb4daff1cd1a
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-04-03 12:51:26 +0100

    fix commiting to db bug

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
 
@@ -18,14 +19,26 @@ class Form(db.Model):
     occupation = db.Column(db.String(25))
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    print(request.method)
+    if request.method == "POST":
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
         date = request.form['date']
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form['occupation']
+
+        form = Form(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            date_created=date_obj,
+            occupation=occupation
+        )
+        db.session.add(form)
+        db.session.commit()
     return render_template('index.html')
 
 
